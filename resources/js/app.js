a61c2c72ada6d90f19e0a06de0ba0783cda5e32d
Francis Onah <francis4u1991@yahoo.com>
2020-11-18 09:04:04 +0100

    Vform, Api Resource and insert user data

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,15 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform'
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+
 
 const routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::apiResources(['user' => 'API/UsersController']);
+
+Route::apiResources([
+    'user' => App\Http\Controllers\API\UsersController::class,
+]);
